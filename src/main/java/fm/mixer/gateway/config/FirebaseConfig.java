42b65bf3c6f20f23023489519bee1456d3f4d64a
MixerFM <154316946+musicmixerai@users.noreply.github.com>
2025-02-25 12:52:35 +0100

    Create FirebaseConfig.java

---
+++ src/main/java/fm/mixer/gateway/config/FirebaseConfig.java
@@ -0,0 +1,34 @@
+package fm.mixer.gateway.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${firebase.credentials.path}")
+    private String firebaseCredentialsPath;
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(
+                new ClassPathResource(firebaseCredentialsPath).getInputStream()
+            );
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .build();
+
+            return FirebaseApp.initializeApp(options);
+        }
+        return FirebaseApp.getInstance();
+    }
+}
